fcf5a2124f59dc8b930d1137dbc1a326b403f3cf
LudyRamos <137777175+LudyRamos@users.noreply.github.com>
2023-11-21 06:28:16 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const sr = ScrollReveal ({
+    distance: '40px',
+    duration: 2050,
+    delay: 200,
+    reset: true
+});
+
+sr.reveal('.home-text',{origin:'top'});
+sr.reveal('.hedy, .infos',{origin:'bottom'});
+sr.reveal('.img, h2',{origin:'top'});
